9c7c4691f0bd97d9e7d5250cb1d0a12825503862
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-09-18 09:49:19 +0900

    プロフィールのTOP page実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
-  get 'top/index', to: 'top#index'
+
+  # devise
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  end
+
+  # Profile
+  get 'users/:id', to: 'users/profile#show', as: 'user'
+
 end
