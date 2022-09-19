d8a5d2e43b62ab2131a71ba5c98c68876823aa58
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-09-19 15:41:15 +0900

    スワイプ実装完了

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,12 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # ログイン後のリダイレクト先 変更
+  # atter_sign_in_path_forメソッド オーバーライド
+  protected
+
+  def after_sign_in_path_for(resource)
+    users_path
+  end
 end
--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -1,10 +1,9 @@
 /**
  * /usersのスワイプ処理を実装する
  * 
+ * マッチング画面でのみ実行
  * Hammer.jsを使用
  */
-
-// /usersパスでのみ実行
 if(location.pathname == "/users" || location.pathname == '/users/') {
     $(function () {
 
@@ -16,25 +15,37 @@ if(location.pathname == "/users" || location.pathname == '/users/') {
          * 
          * z-indexの調整、表示位置、透明度の調整
          */ 
-         function initCards() {
+        function initCards() {
 
+            // カード一覧取得（スワイプ済みのカードを除く）
             let newCards = document.querySelectorAll('.swipe--card:not(.removed)');
 
+            // カードの属性変更
             newCards.forEach(function (card, index) {
               card.style.zIndex = allCards.length - index;
               card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
               card.style.opacity = (10 - index) / 10;
             });
-          }
 
+            // ユーザーが存在しない場合
+            if (newCards.length == 0) {
+                $(".no-user").addClass("is-active");
+            }
+
+        }
+
+        // 初期化
         initCards();
 
+        /**
+         * イベント登録
+         */
         allCards.forEach(function (el) {
             // Hammerインスタンス作成
             let hammertime = new Hammer(el);
             
             /**
-             * panイベントの登録（要素をクリックした状態での画面操作）
+             * panイベント（要素をクリックした状態での画面操作）
              */
             hammertime.on('pan', function (event) {
 
@@ -57,7 +68,7 @@ if(location.pathname == "/users" || location.pathname == '/users/') {
             });
 
             /**
-             * panendイベントの登録（要素から指を離した時）
+             * panendイベント（要素から指を離した時）
              */
             hammertime.on('panend', function (event) {
                 el.classList.remove('moving');
@@ -72,22 +83,62 @@ if(location.pathname == "/users" || location.pathname == '/users/') {
                 event.target.classList.toggle('removed', !keep);
         
                 if (keep) {
-                event.target.style.transform = '';
-            } else {
-                let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth) + 100;
-                let toX = event.deltaX > 0 ? endX : -endX;
-                let endY = Math.abs(event.velocityY) * moveOutWidth;
-                let toY = event.deltaY > 0 ? endY : -endY;
-                let xMulti = event.deltaX * 0.03;
-                let yMulti = event.deltaY / 80;
-                let rotate = xMulti * yMulti;
-    
-                event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
-    
-                initCards();
-            }
+                    event.target.style.transform = '';
+                } else {
+                    let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth) + 100;
+                    let toX = event.deltaX > 0 ? endX : -endX;
+                    let endY = Math.abs(event.velocityY) * moveOutWidth;
+                    let toY = event.deltaY > 0 ? endY : -endY;
+                    let xMulti = event.deltaX * 0.03;
+                    let yMulti = event.deltaY / 80;
+                    let rotate = xMulti * yMulti;
+        
+                    event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+        
+                    initCards();
+                }
             });
 
         });
+
+        // 手動でボタンをクリックした時
+        // LIKEボタン
+        $('#like').on('click', function() {
+            createButtonListener("like");
+        })
+    
+        // NOT LIKEボタン
+        $('#dislike').on('click', function() {
+            createButtonListener("dislike");
+        })
+
+        /**
+         * リアクションクリック後のカードの移動
+         * 
+         * LIKE     右で移動
+         * DIS LIKE 左で移動 
+         * 
+         * @param {*} reaction 
+         * @returns 
+         */
+        function createButtonListener(reaction) {
+            let cards = document.querySelectorAll('.swipe--card:not(.removed)');
+            if (!cards.length) return false;
+
+            let moveOutWidth = document.body.clientWidth * 2;
+            let card = cards[0];
+            card.classList.add('removed');
+
+            // カードの移動
+            if (reaction == "like") {
+                card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+            } else {
+                card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+            }
+
+            // カード初期化
+            initCards();
+        }
+    
     });
 }
