af315c35980ef4670e4f16e1efacae47717edad4
Pen Peen <ikegami1022.h@gmail.com>
2025-04-28 23:52:39 +0900

    エントリポイントスクリプトの更新、Webpackerのコンパイル手順を修正。JavaScriptファイル内のインポート順序を整理し、SCSSファイルのインポートパスを修正。アプリケーションレイアウトから不要なスタイルシートパックタグを削除。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,23 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// bootstrapの使用
-import 'bootstrap';
-import '../stylesheets/application';
-import '@fortawesome/fontawesome-free/js/all';
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// スタイルシートとプラグインのインポート
+import '../stylesheets/application.scss';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all';
+
 import 'hammerjs';
 
+// 各JavaScriptモジュールのインポート
 import '../src/profile_image_upload';
 import '../src/swipe';
-
-// require("src/profile_image_upload");
-// require("src/swipe")
 
 Rails.start()
 Turbolinks.start()
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,12 @@ bundle check || bundle install --path vendor/bundle
 # JavaScript依存関係を確認して修正
 yarn install --check-files
 
+# Webpackerをコンパイル
+RAILS_ENV=development bundle exec rails webpacker:clobber
+RAILS_ENV=development bundle exec rails webpacker:compile
+
 # データベースのマイグレーションを実行
 bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
-
-# Webpackerをコンパイル
-bundle exec rails webpacker:compile || true
 
 # コンテナのメインプロセスとしてRailsサーバを実行
 exec "$@"
